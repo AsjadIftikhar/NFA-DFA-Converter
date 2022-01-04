64392bd2dca58e0eef9de14c68fce4f9567e4a00
kai <iftikharasjad@gmail.com>
2022-01-04 14:18:02 +0500

    File Handler Class Added!

---
+++ src/com/converter/FileHandler.java
@@ -0,0 +1,12 @@
+package com.converter;
+
+public class FileHandler {
+
+    //Add Required Class members here like print writer etc
+
+    // Add a suitable Constructor
+
+    // Add Two methods:
+    // 1. To Read a NDFSA from File in txt format
+    // 2. Write a DFA transition table back to another File in txt format
+}
--- src/com/converter/Main.java
+++ src/com/converter/Main.java
@@ -3,6 +3,16 @@ package com.converter;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	    // The Program Execution Starts from Here
+
+        //Step 1: Read NDFSA-null from File:
+
+        //Step 2: Convert NDFSA-null to NDFSA:
+
+        //Step 3: Convert NDFSA to DFA
+
+        //Step 4: Minimize DFA
+
+        //Step 5: Run DFA to get Accept/Reject
     }
 }
