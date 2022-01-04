cd1cb8ce884dd454e39a915c9bff207834e03d6b
kai <iftikharasjad@gmail.com>
2022-01-04 14:33:50 +0500

    Basic Code Skeleton Implemented

---
+++ src/com/converter/DFA.java
@@ -0,0 +1,7 @@
+package com.converter;
+
+// This Class must implement two methods:
+// 1. A DFA minimizer: to minimize a DFA
+// 2. A DFA executor: to run a sample string on the DFA
+public class DFA {
+}
---
+++ src/com/converter/NFA.java
@@ -0,0 +1,11 @@
+package com.converter;
+
+// This Class must implement two methods:
+// 1. A converter which converts nfa-null to nfa
+// 2. A converter which converts nfa to dfa
+
+//Other helping methods and class members could be used
+
+public class NFA {
+
+}
