8b47de6da9269e860a616d5c2055deaa7b180c7e
kai <iftikharasjad@gmail.com>
2022-01-05 11:24:12 +0500

    DFA Class Setup

--- src/com/converter/DFA.java
+++ src/com/converter/DFA.java
@@ -1,7 +1,21 @@
 package com.converter;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+
 // This Class must implement two methods:
 // 1. A DFA minimizer: to minimize a DFA
 // 2. A DFA executor: to run a sample string on the DFA
 public class DFA {
+
+    public DFA() {}
+
+    public HashMap<ArrayList<String>, State> MINIMIZE_DFA(HashMap<ArrayList<String>, State> converted_dfa) {
+
+        return null;
+    }
+
+    public boolean EXECUTE_ON_DFA(HashMap<ArrayList<String>, State> DFA) {
+        return false;
+    }
 }
